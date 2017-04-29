979e3303967994a4e922582096dba5425c05c916
Doug McLaughlin <dmclaughlin@homeadvisor.com>
2017-04-29 11:09:53 -0700

    stuff

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -17,7 +17,7 @@
 int * get_fd_arg(struct intr_frame *f);
 char ** get_buffer_arg(struct intr_frame *f);
 unsigned* get_size_arg(struct intr_frame *f);
-void is_valid_buffer(char ** buffer, unsigned * size);
+void is_valid_buffer(void* buffer, unsigned * size);
 static void syscall_handler (struct intr_frame *);
 static struct lock file_lock;
 void
