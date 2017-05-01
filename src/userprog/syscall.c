6a715de329be31c6232d1ef236d29edc31206105
Doug McLaughlin <dmclaughlin@homeadvisor.com>
2017-05-01 11:59:18 -0600

    work

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -110,7 +110,7 @@ syscall_handler (struct intr_frame *f)
     case SYS_READ: {
       // Retrieve arguments and is_valid.
       int* fd = get_fd_arg(f);
-      char** raw = (char**) ((char*)f->esp+8);
+      char** raw = get_buffer_arg(f);
       unsigned* size = (unsigned*) ((char*)f->esp + 12);
       is_valid(fd);
       is_valid(raw);
