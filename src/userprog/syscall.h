658c8e51b7d390aa3e7dee5d105835517a2192d1
DESKTOP-IQUAV67\Joshua Rocha <jrocha4@msudenver.edu>
2017-05-02 18:47:38 -0600

    working

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -6,8 +6,6 @@ void exit(int);
 // Accepts an address, calls exit(-1) if the address is out of range
 void validate(void* addr);
 
-// Reads from a file represented by fd.
-int s_read(int fd, char* buf, unsigned size);
 
 // Writes to a file represented by fd.
 int s_write(int fd, char* buf, unsigned size);
@@ -15,8 +13,6 @@ int s_write(int fd, char* buf, unsigned size);
 // Creates a file with name 'file' and size 'size'.
 int s_create(char* file, unsigned size);
 
-// Opens the file with filename 'file'.
-int s_open(char* file);
 
 // Closes file represented by fd.
 void s_close(int fd);
@@ -27,8 +23,6 @@ void s_seek(int fd, unsigned position);
 // Returns the location of the cursor in 'fd'.
 unsigned s_tell(int fd);
 
-// Returns the filesize of the file represented by fd.
-int s_filesize(int fd);
 
 
 int s_remove(char* name);
