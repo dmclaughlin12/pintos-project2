ca11afcfb8543dd5a7671392c0e9184307068bb5
DESKTOP-IQUAV67\Joshua Rocha <jrocha4@msudenver.edu>
2017-05-05 10:45:59 -0600

    might be working

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -479,9 +479,9 @@ init_thread (struct thread *t, const char *name, int priority)
   t->magic = THREAD_MAGIC;
   t->next_fd = 2;
   // Initialize list of children for the thread.
-  list_init(&t->children);
+  list_init(&t->list_of_children);
   // Initialize list of files for the process.
-  list_init(&t->files);
+  list_init(&t->open_files);
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
